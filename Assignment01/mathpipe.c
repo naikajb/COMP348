d7f7b5ac1ce284adfa85db29b030574d18ab5cf8
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-05-24 16:58:18 -0400

    added execution/compilation comment

--- Assignment01/mathpipe.c
+++ Assignment01/mathpipe.c
@@ -3,6 +3,13 @@
     Assignment 1 - Due June 5th 2023 @ midnight 
     Naika Jean-Baptiste (40227367)
 */
+
+/*
+        compilation: gcc -Wall aggregate.c singular.c mathpipe.c
+        execution: 
+            ex  cat sample.txt | mathpipe sum -size = 2
+            ex  cat sample.txt | mathpipe -size=1 FILTER GEQ 10 | mathpipe SUM
+*/
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
