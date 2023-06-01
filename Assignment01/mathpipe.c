1cf6fa3782cdb35f4c4c9f3854a3c01540d15d78
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-05-31 23:13:40 -0400

    implemented reading from stdin the values to use

--- Assignment01/mathpipe.c
+++ Assignment01/mathpipe.c
@@ -1,48 +1,46 @@
 /*
     COMP 348 - Programming Languages
-    Assignment 1 - Due June 5th 2023 @ midnight 
+    Assignment 1 - Due June 5th 2023 @ midnight
     Naika Jean-Baptiste (40227367)
 */
 
 /*
         compilation: gcc -Wall aggregate.c singular.c mathpipe.c
-        execution: 
+        execution:
                 ex ==> cat sample.txt | mathpipe sum -size = 2
                 ex ==> cat sample.txt | mathpipe -size=1 FILTER GEQ 10 | mathpipe SUM
 */
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
 
-   int size = 0;
-   int prec = 0;
+    char buffer = 0;
 
-   //printf("%s\n", argv[0]);
-   char buffer = 0;
-   for(int i = 1; i < argc; i++){
+    // space for values in the input file
+    double *valuesArr = (double *)malloc(256 * sizeof(double));
+    double value = 0;
+    int scanned = 0;
+    int n = 0;
 
-    const char * arg = argv[i];
-    if(strstr(arg,"sum")){
-        printf("hello sum\n");
-    }else if (strstr(arg,"-size=")){
-
-        printf("hello -size= \n");
-
+    //reads a line by line from stdin 
+    //in this case will read a line from sample.txt file 
+    while (fgets(&buffer, 1000, stdin) != NULL)
+    {
+        int offset = 0;
+        
+        //this will look at the line currently in the buffer
+        //each double at a time is analysed and put in the array "valuesArr"
+        while(sscanf(&buffer + offset, "%lf%n", &value, &scanned) == 1){
+            valuesArr[n] = value;
+            //printf("[%d]\t --> \t%f\n", n,valuesArr[n]);
+            n++;
+            offset += scanned;
+        }
     }
-    //printf("[%d]\t%s\n", i,argv[i]);
-     /*if (strcmp(argv[i],"sum")){
-        printf("hello");
-     }*/
-
-   }
-   
-   
-   /*while(fgets(&buffer, 10,stdin) != NULL){
-        printf("%s",&buffer);
-   }*/
-   
+    free(valuesArr);
 
     return 0;
 }
