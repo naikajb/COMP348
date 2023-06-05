f1c77cbed6a55c0a9e8ca8ac7c6d5b5584038c98
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-06-05 10:26:01 -0400

    gets the value for filter function

--- Assignment01/mathpipe.c
+++ Assignment01/mathpipe.c
@@ -19,30 +19,35 @@
 
 int prec = 0;
 int size = -1;
-int singFuncIdx;//index at which the singular function is;
+int singFuncIdx; // index at which the singular function is;
+int singFuncValue;
 bool isAggregate = false;
 bool isSingular = false;
-char* aggFunc;
-char* singFunc;
-char* filterType;
+char *aggFunc;
+char *singFunc;
+char *filterType;
+char *filterOpt;
 
 void handleCmdLine(int argc, char const **argv);
-void checkAggregate(char* funcName);
-void checkSingular(char* funcName, const char** args);
+void checkAggregate(char *funcName);
+void checkSingular(char *funcName, const char **args);
 void performActions(char const **argv, int size);
 
 int main(int argc, char const *argv[])
 {
 
-    if(argc == 1 ){
+    if (argc == 1)
+    {
         printf("usage: cat <input file> | mathpipe <function name> <optional parameters> ");
         return 0;
-    }else{
+    }
+    else
+    {
         handleCmdLine(argc, argv);
     }
 
-    //printf("\n%s", aggFunc);
-   
+    // printf("\n%s", aggFunc);
+
     char buffer = 0;
     // space for values in the input file
 
@@ -51,146 +56,249 @@ int main(int argc, char const *argv[])
     int scanned = 0;
     int n = 0;
 
-    //reads a line by line from stdin 
-    //in this case will read a line from sample.txt file 
+    // reads a line by line from stdin
+    // in this case will read a line from sample.txt file
     while (fgets(&buffer, 1000, stdin) != NULL)
     {
-        if(size == -1){
+        if (size == -1)
+        {
             int offset = 0;
-            //this will look at the line currently in the buffer
-            //each double at a time is analysed and put in the array "valuesArr"
-            while(sscanf(&buffer + offset, "%lf%n", &value, &scanned) == 1){
+            // this will look at the line currently in the buffer
+            // each double at a time is analysed and put in the array "valuesArr"
+            while (sscanf(&buffer + offset, "%lf%n", &value, &scanned) == 1)
+            {
                 valuesArr[n] = value;
-                printf("[%d]\t --> \t%f\n", n,valuesArr[n]);
+                printf("[%d]\t --> \t%f\n", n, valuesArr[n]);
                 n++;
                 offset += scanned;
                 size = n;
             }
-        }else{
+        }
+        else
+        {
             int offset = 0;
-            //this will look at the line currently in the buffer
-            //each double at a time is analysed and put in the array "valuesArr"
-            while(sscanf(&buffer + offset, "%lf%n", &value, &scanned) == 1){
+            // this will look at the line currently in the buffer
+            // each double at a time is analysed and put in the array "valuesArr"
+            while (sscanf(&buffer + offset, "%lf%n", &value, &scanned) == 1)
+            {
                 valuesArr[n] = value;
-                printf("[%d]\t --> \t%f\n", n,valuesArr[n]);
+                printf("[%d]\t --> \t%f\n", n, valuesArr[n]);
                 n++;
                 offset += scanned;
             }
         }
-        
-        //TODO check for the precision and size before performing actions method
-        //performActions(valuesArr,);
+
+        // TODO check for the precision and size before performing actions method
+        // performActions(valuesArr,);
     }
     free(valuesArr);
 
     return 0;
 }
 
-void checkAggregate(char* funcName){
-    if (isAggregate == false){
+void checkAggregate(char *funcName)
+{
+    if (isAggregate == false)
+    {
         isAggregate = true;
         aggFunc = funcName;
-        printf("the chosen function is %s", funcName);
+        // printf("the chosen function is %s", funcName);
+    }
+    else
+    {
+        fprintf(stderr, "\nProblem: your are trying to use two aggregate functions at once\n\t-->current specificed function: %s\n\t-->trying to use function: %s", aggFunc, funcName);
+    }
+}
+
+void checkSingular(char *funcName, const char **args)
+{
+    if (isSingular == false)
+    {
+        if (strstr(funcName, "FILTER") != NULL)
+        {
+            isSingular = true;
+            printf("\nmissing implemetation of filter func for options: %s %s", args[singFuncIdx + 1], args[singFuncIdx + 2]);
+            singFunc = "filter";
+        }
+        else if (strstr(funcName, "PRINT") != NULL)
+        {
+            isSingular = true;
+            printf("\nmissing implemetation of print func");
+            singFunc = "print";
+        }
+        else if (strstr(funcName, "SHIFT") != NULL)
+        {
+            isSingular = true;
+            singFunc = "shift";
+        }
     }else{
-        fprintf(stderr, "\nProblem: your are trying to use two aggregate functions at once\n\t-->current specificed function: %s\n\t-->trying to use function: %s", aggFunc,funcName);
+        fprintf(stderr, "can have two singular functions");
     }
 }
 
-void checkSingular(char* funcName,const char** args){
-    if(strstr(funcName,"FILTER") != NULL){
-        isSingular = true;
-        printf("\nmissing implemetation of filter func for options: %s %s", args[singFuncIdx +1], args[singFuncIdx +2]);
-        singFunc = "filter";
-    }else if(strstr(funcName,"PRINT") != NULL ){
-        isSingular = true;
-        printf("\nmissing implemetation of print func");
-        singFunc = "print";
-    }else if(strstr(funcName,"SHIFT") != NULL){
-        isSingular = true;
-        singFunc = "shift";
+void performActions(char const **argv, int size)
+{
+    if (isAggregate)
+    {
+        // aggregrate(aggFunc,a, size);
     }
-
-}
-
-void performActions(char const** argv, int size){
-    if(isAggregate){
-        //aggregrate(aggFunc,a, size);
-    }
-    else if(isSingular){
-        if (strcmp(singFunc,"FILTER") == 0)
+    else if (isSingular)
+    {
+        if (strcmp(singFunc, "FILTER") == 0)
         {
             /* code */
         }
-        else if (strcmp(singFunc,"PRINT") == 0){
-            //print(a)
-        }else if (strcmp(singFunc,"SHIFT") == 0){
-            //shift(a);
+        else if (strcmp(singFunc, "PRINT") == 0)
+        {
+            // print(a)
         }
-        
+        else if (strcmp(singFunc, "SHIFT") == 0)
+        {
+            // shift(a);
+        }
     }
 }
 
-void handleCmdLine(int argc, char const **argv){
+void handleCmdLine(int argc, char const **argv)
+{
     printf("Function to handle command line inputs:\n");
 
-    for( int i = 1; i < argc; i++)
+    for (int i = 1; i < argc; i++)
     {
-        if(strcasecmp(argv[i],"sum") == 0)
+        if (strcasecmp(argv[i], "sum") == 0)
         {
-           // printf("first function is sum");
+            // printf("first function is sum");
             checkAggregate("SUM");
-            
-        }else if (strcasecmp(argv[i], "PAVG") == 0)
+        }
+        else if (strcasecmp(argv[i], "PAVG") == 0)
         {
-            //printf("func is PAVG");
+            // printf("func is PAVG");
             checkAggregate("PAVG");
-            
         }
-        else if (strcasecmp(argv[i],"AVG") == 0)
+        else if (strcasecmp(argv[i], "AVG") == 0)
         {
-            //printf("func is AVG");
+            // printf("func is AVG");
             checkAggregate("AVG");
-
         }
-        else if (strcasecmp(argv[i],"COUNT") == 0)
+        else if (strcasecmp(argv[i], "COUNT") == 0)
         {
             checkAggregate("COUNT");
         }
-        else if(strcasecmp(argv[i], "MAX") == 0)
+        else if (strcasecmp(argv[i], "MAX") == 0)
         {
             checkAggregate("MAX");
         }
-        else if(strcasecmp(argv[i], "MIN") == 0)
+        else if (strcasecmp(argv[i], "MIN") == 0)
         {
             checkAggregate("MIN");
         }
-        else if(strcasecmp(argv[i], "FILTER") == 0)
+        else if (strcasecmp(argv[i], "FILTER") == 0)
+        {
+            if (strcasecmp(argv[i + 1], "EQ") == 0)
+            {
+
+                if (sscanf(argv[i + 2], "%i", &singFuncValue) == 1)
+                {
+                    filterOpt = "EQ";
+                    singFuncIdx = i;
+                    checkSingular("FILTER", argv);
+                }
+            }
+            else if (strcasecmp(argv[i + 1], "GEQ") == 0)
+            {
+
+                if (sscanf(argv[i + 2], "%i", &singFuncValue) == 1)
+                {
+                    singFuncIdx = i;
+                    filterOpt = "GEQ";
+                    checkSingular("FILTER", argv);
+                }
+            }
+            else if (strcasecmp(argv[i + 1], "NEQ") == 0)
+            {
+
+                if (sscanf(argv[i + 2], "%i", &singFuncValue) == 1)
+                {
+                    filterOpt = "NEQ";
+                    singFuncIdx = i;
+                    checkSingular("FILTER", argv);
+                }
+            }
+            else if (strcasecmp(argv[i + 1], "LEQ") == 0)
+            {
+                if (sscanf(argv[i + 2], "%i", &singFuncValue) == 1)
+                {
+                    filterOpt = "LEQ";
+                    singFuncIdx = i;
+                    checkSingular("FILTER", argv);
+                }
+            }
+            else if (strcasecmp(argv[i + 1], "LESS") == 0)
+            {
+
+                if (sscanf(argv[i + 2], "%i", &singFuncValue) == 1)
+                {
+                    singFuncIdx = i;
+                    filterOpt = "LESS";
+                    checkSingular("FILTER", argv);
+                }
+            }
+            else if (strcasecmp(argv[i + 1], "GREATER") == 0)
+            {
+
+                if (sscanf(argv[i + 2], "%i", &singFuncValue) == 1)
+                {
+                    filterOpt = "GREATER";
+                    singFuncIdx = i;
+                    checkSingular("FILTER", argv);
+                }
+            }
+            else
+            {
+                fprintf(stderr, "smtg went wrong with trying initialize the FILTER command");
+            }
+        }
+        else if (strcasecmp(argv[i], "PRINT") == 0)
         {
             singFuncIdx = i;
-            checkSingular("FILTER", argv);
-        }else if (strcasecmp(argv[i], "PRINT") == 0){
-            singFuncIdx = i;
             checkSingular("PRINT", argv);
-        }else if (strcasecmp(argv[i], "SHIFT") == 0){
+        }
+        else if (strcasecmp(argv[i], "SHIFT") == 0)
+        {
             singFuncIdx = i;
             checkSingular("SHIFT", argv);
-        }else if(strstr(argv[i], "-size=") != NULL ){
+        }
+        else if (strstr(argv[i], "-size=") != NULL)
+        {
 
-            //scans the string for the integer representing size
-            while(sscanf(argv[i],"-size=%d",&size)== 1 ){
-                printf("Specified size of arrays --> %d",size);
-                break;
-            }
-
-        }else if (strstr(argv[i], "-prec=") != NULL ){
-
-            //scans the string for the integer representing precision
-            while(sscanf(argv[i],"-prec=%d",&prec)== 1 ){
-                printf("Specified precision of arrays --> %d",prec);
+            // scans the string for the integer representing size
+            while (sscanf(argv[i], "-size=%d", &size) == 1)
+            {
+                printf("Specified size of arrays --> %d", size);
                 break;
             }
         }
-        
-        printf("\n\t[%d]\t%s\n", i,argv[i]);
+        else if (strstr(argv[i], "-prec=") != NULL)
+        {
+
+            // scans the string for the integer representing precision
+            while (sscanf(argv[i], "-prec=%d", &prec) == 1)
+            {
+                printf("Specified precision of arrays --> %d", prec);
+                break;
+            }
+        }
+
+        printf("\n\t[%d]\t%s\n", i, argv[i]);
+    }
+
+    if (isAggregate == true)
+    {
+        printf("The chosen aggregate function is %s\n", aggFunc);
+    }
+
+    if (isSingular == true)
+    {
+        printf("The chosen singular function is %s with  option %s and value %i\n", singFunc, filterOpt, singFuncValue);
     }
 }
