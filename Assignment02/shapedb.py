3351694b62ef9dc2bf9c3f777e49dbada8389ccb
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-06-14 15:37:16 -0400

    started shape class

---
+++ Assignment02/shapedb.py
@@ -0,0 +1,27 @@
+# Assignment 02 - Shape Database
+# Naika Jean-Baptiste (40227367)
+# COMP 348 - SUMMER 2023
+
+
+class Shape:
+    children = 0
+    id = 0
+    def __init__():
+        Shape.children += 1
+        id = Shape.children 
+        print("Shape constructor called.")
+
+    def print(self):
+        print("Shape id: " + str(self.id))
+        #print( self.__class__.__name__+ str(id))
+
+    
+
+    # def area(self):
+    #     pass
+
+    # def perimeter(self):
+    #     pass
+
+    # def __str__(self):
+    #     return "x: " + str(self.x) + ", y: " + str(self.y)
