bce0411394249d99baa64f2786e9d62aea095b11
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-06-21 21:25:30 -0400

    added menu display

--- Assignment03/app.clj
+++ Assignment03/app.clj
@@ -1,2 +1,12 @@
-;; (-(+ 3 (* 2 4)) 1)
-println "\t***SIS Menu***"
+(println "*** SIS Menu ***
+         \n----------------
+         \n1. Display Courses
+         \n2. Display Students
+         \n3. Display Grades
+         \n4. Display Student Record
+         \n5. Calculate GPA
+         \n6. Course Average
+         \n7. Exit
+         \newlineter an option?")
+(def x (read-line))
+(println (str "this is what was enetered\"" x "\""))
