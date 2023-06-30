64b03047a1bdffb65fbd6ac232ecca5bc2fdd552
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-06-30 10:52:27 -0400

    Update app.clj

--- Assignment03/app.clj/src/app/app.clj
+++ Assignment03/app.clj/src/app/app.clj
@@ -1,10 +1,13 @@
 (ns app
   (:require [db] [menu]))
 
-;; Load data into appropriate data structures
-(def studDB (db/load-data "studs.txt"))
-(def courseDB (db/load-data "courses.txt"))
-(def gradesDB (db/load-data "grades.txt"))
+;; Load data from the file
+(def studDB 
+  (db/load-data "studs.txt"))
+(def courseDB 
+  (db/load-data "courses.txt"))
+(def gradesDB
+  (db/load-data "grades.txt"))
 
 ;; Start the main menu loop
-(menu/main-menuloop)
+(menu/main-loop)
